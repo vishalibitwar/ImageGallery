9ae791da92df8c00d9ac52cc116475048fda2a89
vishalibitwar <vish31ibitwar@gmail.com>
2020-06-05 16:57:09 +0530

    Updates

---
+++ precache-manifest.ac3e7cab25e96a822132141b34558a6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3edd81a9999e48ff8889d5a4095fb02f",
+    "url": "/ImageGallery/index.html"
+  },
+  {
+    "revision": "6a0bcaf63bf5e5ed24eb",
+    "url": "/ImageGallery/static/css/main.52e2871a.chunk.css"
+  },
+  {
+    "revision": "c98cf3c44f6cd9f12e04",
+    "url": "/ImageGallery/static/js/2.5f38b4b7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ImageGallery/static/js/2.5f38b4b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a0bcaf63bf5e5ed24eb",
+    "url": "/ImageGallery/static/js/main.825b0c21.chunk.js"
+  },
+  {
+    "revision": "8821d546c7414b5eaaff",
+    "url": "/ImageGallery/static/js/runtime-main.49271329.js"
+  }
+]);
